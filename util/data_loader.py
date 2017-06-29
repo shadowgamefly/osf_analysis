08040d86a2edd7d52dd38ecfb45531ca5c2e4811
Jerry Sun <jerrysun@Admins-MacBook-Air.local>
2017-06-29 11:38:29 -0400

    loader modified

--- util/data_loader.py
+++ util/data_loader.py
@@ -2,14 +2,40 @@ import os
 import json
 import pickle
 
-def load_name_data(directory):
+def load_name_data(data_dir):
+    try :
+        with open('gender.pickle', 'rb') as f:
+            sex_dict = pickle.load(f)
+            print('pickle data found and loaded')
+            
+    except (EOFError, FileNotFoundError) as e:
+        if e == EOFError:
+            print('current pickle data is broken')
+            print('reloading...')
+        elif e == FileNotFoundError:
+            print('no loaded data found')
+            print('loading...')
+        sex_dict = process_name_data(data_dir)
+        
+    return sex_dict
+
+def process_name_data(directory):
+    directory = os.path.join(directory, 'names')
     print(directory)
     sex_dict = {}
     total = 0
+    people = 0
     male = 0
     female = 0
+    files = 0
+    count = 0
+    print("Loading", end = "")
     for file in os.listdir(directory):
+        if count % 10 == 0:
+            print(".", end = "")
+        count += 1
         filename = os.fsdecode(file)
+        files += 1
         if filename.endswith(".txt"): 
             with open(os.path.join(directory, file), 'rb') as f:
                 content = f.readlines()
@@ -25,12 +51,14 @@ def load_name_data(directory):
                     else :
                         sex_dict[name] = [0, 0]
                         sex_dict[name][gender] += num
+                    people += num
                     total += 1
         else:
             continue
     with open('gender.pickle', 'wb') as f:
         pickle.dump(sex_dict, f, pickle.HIGHEST_PROTOCOL)
-        print('dictionary has been saved in root directory')
-        print('{:d} names loaded, {:d} males, {:d} females'.format(total,male, female)
+        print('\n')
+        print('dictionary has been saved in root directory as gender.pickle')
+        print('{:d} files found, {:d} names among {:d} people loaded, {:d} male names, {:d} females names'.format(files, total, people, male, female))
+    
     return sex_dict
-
