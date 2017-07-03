3fa9f83de99eadf0fecded678a8e9b3761762dd4
Jerry Sun <jerrysun1007@gmail.com>
2017-07-03 13:34:41 -0400

    restructure menuu-parsing

--- crawler/crawler.py
+++ crawler/crawler.py
@@ -1,6 +1,7 @@
 from urllib import request
 from util import *
-def crawl(subject, page, cache):
+
+def crawl_menu(subject, page, cache):
     url = "http://network.bepress.com/" + subject + "/page"+ str(page)
     response = request.urlopen(url)
     source = response.read().decode("utf-8")
@@ -8,3 +9,11 @@ def crawl(subject, page, cache):
         return url, -1
     write_pickle(subject + '_page' + str(page), 'html', source)
     return url, 1
+
+def crawl_content(url, page, cache):
+    response = request.urlopen(url)
+    source = response.read().decode("utf-8")
+    if response.getcode() != 200 :
+        return url, -1
+    else :
+        return source, 1
--- crawler/run.py
+++ crawler/run.py
@@ -3,16 +3,22 @@ from crawler import *
 from parser import *
 import time
 
-if __name__ == '__main__':
-    dir = os.getcwd()
-    subject = 'law'
+def menu_session(subject, dir):
     q, h, p = init(dir)
     while True:
-        url, status = crawl(subject, p, dir)
+        url, status = crawl_menu(subject, p, dir)
         if status != 1 :
             print("Crawling finished")
             break
         print("{:s} saved".format(url))
+        links = menu_parse(subject + '_page' + str(p) + '.pickle')
+        q += links
         p += 1
+        print(q)
         record_status(q, h, p)
         time.sleep(5)
+
+if __name__ == '__main__':
+    dir = os.getcwd()
+    subject = 'law'
+    menu_session(subject, dir)
